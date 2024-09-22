8436ac3495746fbcd6e40f211e2580795ef722d8
JasonLewis12 <Lewisjason64@yahoo.com>
2024-09-22 17:15:21 -0400

    completed table

---
+++ contact-list/src/ContactRow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ContactRow({ contacts }) {
+  return (
+    <tr>
+      <td>{contacts.name}</td>
+      <td>{contacts.email}</td>
+      <td>{contacts.phone}</td>
+    </tr>
+  );
+}
